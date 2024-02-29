1a0276abb5bf641c8d8361d99755eabdf11db210
Muhammad Shoaib <160269862+shoaibkhan123987@users.noreply.github.com>
2024-02-29 23:03:24 +0500

    Add files via upload

---
+++ 14.Guest_List.ts
@@ -0,0 +1,6 @@
+let Guest_List :string[] = ['Yasir','Abid','Ali'];
+
+for(let i=0; i<Guest_List.length; i++){
+
+    console.log('Dear Mr. ' + Guest_List[i] + ',\n\nit is our pleasure to invite you in our party.\n\nThank You! ')
+}
