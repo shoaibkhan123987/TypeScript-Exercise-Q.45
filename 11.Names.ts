c99c37a06ac725f6a8f1a5ca4cca771b016eeb5c
Muhammad Shoaib <160269862+shoaibkhan123987@users.noreply.github.com>
2024-02-29 22:56:22 +0500

    Add files via upload

---
+++ 11.Names.ts
@@ -0,0 +1,6 @@
+let names :string[] =['shoaib','sabir','usama','asad'];
+// is main hum erry ko use kr rhy hain [] is ko erry kehty hain
+for (let i=0; i<names.length; i++){
+
+    console.log(names[i]);
+}
