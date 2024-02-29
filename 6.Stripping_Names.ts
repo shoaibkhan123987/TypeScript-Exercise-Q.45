126d1abca270816fb3159ec337dfeb73c4c7a44c
Muhammad Shoaib <160269862+shoaibkhan123987@users.noreply.github.com>
2024-02-29 22:49:42 +0500

    Add files via upload

---
+++ 6.Stripping_Names.ts
@@ -0,0 +1,6 @@
+const personName :string ='   \n\tMuhammad Shoaib\t\n   ';
+console.log (personName);
+const whithout_whitespaces :string =personName.trim();
+console.log (whithout_whitespaces);
+
+
