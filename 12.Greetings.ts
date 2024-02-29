7d73ba94fa99a61d61e9ef5d55e7ac0a510a8510
Muhammad Shoaib <160269862+shoaibkhan123987@users.noreply.github.com>
2024-02-29 22:59:21 +0500

    Add files via upload

---
+++ 12.Greetings.ts
@@ -0,0 +1,9 @@
+let names :string[] =['Shoaib','Sabir','Usama','Asad'];
+// is main hum erry ko use kr rhy hain [] is ko erry kehty hain.
+
+const message :string='Welcome to the party: '
+
+for (let i=0; i<names.length; i++){
+
+    console.log(message + names[i]);
+}
