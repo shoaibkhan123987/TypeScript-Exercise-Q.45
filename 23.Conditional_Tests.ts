1f4c4de3827bcadac7de3ebf946dcdf0065c1505
Muhammad Shoaib <160269862+shoaibkhan123987@users.noreply.github.com>
2024-02-29 23:15:05 +0500

    Add files via upload

---
+++ 23.Conditional_Tests.ts
@@ -0,0 +1,62 @@
+//  Write a series of conditional tests.
+//creating a car variable.
+let car :string = 'subaru';
+
+// is = ka matlb h k hm kisi value ko set kr rhy hain .
+
+//aur is == ka mtlb h k hm kisi ko check kr rhy hain .
+
+// and === is ka mtlb hai k hm kisi value ko uski datatype k sath check kr rhy hain.
+
+// Test No: 1.
+console.log("Is car == 'subaru'? I predict True.");
+
+console.log(car == 'subaru');
+
+// Test No: 2.
+console.log("Is car === 'subaru'? I predict True.");
+
+console.log(car === 'subaru');
+
+
+// != agr hm equal= k sath ! ye sign laga dein tu is ka mtlb not equal hai .
+
+// Test No: 3.
+console.log("Is car != 'Honda'? I predict True.");
+
+console.log(car != 'Honda');
+
+// Test No: 4.
+console.log("Is car !== 'Suzuki'? I predict True.");
+
+console.log(car !== 'Suzuki');
+
+// Test No: 5.
+console.log("Is car.upper case== 'SUBARU'? I predict True.");
+
+console.log(car.toUpperCase( )== 'SUBARU');
+
+// Test No: 6.
+console.log("Is car == 'SUBARU'? I predict False.");
+
+console.log(car == 'SUBARU');
+
+// Test No: 7.
+console.log("Is car === 'SUBARU'? I predict False.");
+
+console.log(car === 'SUBARU');
+
+// Test No: 8.
+console.log("Is car === 'Honda'? I predict False.");
+
+console.log(car === 'Honda');
+
+// Test No: 9.
+console.log("Is car === 'Suzuki'? I predict False.");
+
+console.log(car === 'Suzuki');
+
+// Test No: 10.
+console.log("Is car === 'Bike'? I predict False.");
+
+console.log(car === 'Bike');
