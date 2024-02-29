233bf9f51c93184ebdd99ee9432e3bdc0a0ba98a
Muhammad Shoaib <160269862+shoaibkhan123987@users.noreply.github.com>
2024-02-29 23:14:05 +0500

    Add files via upload

---
+++ 22.Intentional_Error.ts
@@ -0,0 +1,9 @@
+// If you haven’t received an array index error in one of your programs yet, try to make one happen.
+//crating array
+let newArray :string[] = ['Shoaib','Yasir','Saleem','Sajid'];
+//accsing arrayby indix error.
+console.log(newArray[5]);
+
+// correct the error
+//Make sure you correct the error before closing the program.
+console.log(newArray);
