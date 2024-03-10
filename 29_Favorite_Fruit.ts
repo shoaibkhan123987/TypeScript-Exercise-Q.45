421bdf3c26282335b3e0c25abbedd8be5d9f4b25
Muhammad Shoaib <160269862+shoaibkhan123987@users.noreply.github.com>
2024-03-10 22:22:52 +0500

    Add files via upload

---
+++ 29_Favorite_Fruit.ts
@@ -0,0 +1,21 @@
+let favorite_fruit :string [] = ['mango','apple','bananna'];
+
+if (favorite_fruit.includes('mango')){
+    console.log('I realy like mango!');
+}
+
+if (favorite_fruit.includes('apple')){
+    console.log('I realy like apple!');
+}
+
+if (favorite_fruit.includes('bananna')){
+    console.log('I realy like bananna!');
+}
+
+if (favorite_fruit.includes('orange')){
+    console.log('I realy like orange!');
+}
+
+if (favorite_fruit.includes('strawbery')){
+    console.log('I realy like strawbery!');
+}
